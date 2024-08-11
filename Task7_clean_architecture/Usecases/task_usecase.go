c7517ca3be2fd28323c3cb15eb2da43a95f2bef0
Johna210 <solomonjohna21@gmail.com>
2024-08-11 21:55:44 +0300

    Completed task usecases

--- Task7_clean_architecture/Usecases/task_usecase.go
+++ Task7_clean_architecture/Usecases/task_usecase.go
@@ -1,1 +1,50 @@
 package usecases
+
+import (
+	"context"
+	"time"
+
+	domain "github.com/Johna210/A2SV-Backend-Track/Track7_clean_architecture/Domain"
+)
+
+type taskUsecase struct {
+	taskRepository domain.TaskRepository
+	contextTimeout time.Duration
+}
+
+func NewTaskUsecase(taskRepository domain.TaskRepository, timeout time.Duration) domain.TaskUsecase {
+	return &taskUsecase{
+		taskRepository: taskRepository,
+		contextTimeout: timeout,
+	}
+}
+
+func (tu *taskUsecase) CreateTask(c context.Context, task *domain.Task) error {
+	ctx, cancel := context.WithTimeout(c, tu.contextTimeout)
+	defer cancel()
+	return tu.taskRepository.CreateTask(ctx, task)
+}
+
+func (tu *taskUsecase) Fetch(c context.Context) ([]domain.Task, error) {
+	ctx, cancel := context.WithTimeout(c, tu.contextTimeout)
+	defer cancel()
+	return tu.taskRepository.Fetch(ctx)
+}
+
+func (tu *taskUsecase) GetByID(c context.Context, id string) (domain.Task, error) {
+	ctx, cancel := context.WithTimeout(c, tu.contextTimeout)
+	defer cancel()
+	return tu.taskRepository.GetByID(ctx, id)
+}
+
+func (tu *taskUsecase) UpdateTask(c context.Context, task *domain.TaskUpdate, id string) (domain.Task, error) {
+	ctx, cancel := context.WithTimeout(c, tu.contextTimeout)
+	defer cancel()
+	return tu.taskRepository.UpdateTask(ctx, task, id)
+}
+
+func (tu *taskUsecase) DeleteTask(c context.Context, id string) error {
+	ctx, cancel := context.WithTimeout(c, tu.contextTimeout)
+	defer cancel()
+	return tu.taskRepository.DeleteTask(ctx, id)
+}
