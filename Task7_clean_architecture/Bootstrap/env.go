766d50ed636a0a8cd123f95aacee5d2be9bde8cb
Johna210 <solomonjohna21@gmail.com>
2024-08-11 23:49:03 +0300

    Implemented app bootstrap

---
+++ Task7_clean_architecture/Bootstrap/env.go
@@ -0,0 +1,31 @@
+package bootstrap
+
+type Env struct {
+	AppEnv                string `mapstructure:"APP_ENV"`
+	ServerAddress         string `mapstructure:"SERVER_ADDRESS"`
+	ContextTimeout        int    `mapstructure:"CONTEXT_TIMEOUT"`
+	DBHost                string `mapstructure:"DB_HOST"`
+	DBPort                string `mapstructure:"DB_PORT"`
+	DBUser                string `mapstructure:"DB_USER"`
+	DBPass                string `mapstructure:"DB_PASS"`
+	DBName                string `mapstructure:"DB_NAME"`
+	AccessTokenExpiryHour int    `mapstructure:"ACCESS_TOKEN_EXPIRY_HOUR"`
+	AccessTokenSecret     string `mapstructure:"ACCESS_TOKEN"`
+}
+
+func NewEnv() *Env {
+	env := Env{
+		AppEnv:                "development",
+		ServerAddress:         "localhost:4000",
+		ContextTimeout:        100,
+		DBHost:                "localhost",
+		DBPort:                "27017",
+		DBUser:                "",
+		DBPass:                "",
+		DBName:                "tasks",
+		AccessTokenExpiryHour: 24,
+		AccessTokenSecret:     "asfgherhwegsdcvds",
+	}
+
+	return &env
+}
--- Task7_clean_architecture/Delivery/controllers/signup_controller.go
+++ Task7_clean_architecture/Delivery/controllers/signup_controller.go
@@ -1,1 +1,4 @@
 package controllers
+
+type SignupController struct {
+}
